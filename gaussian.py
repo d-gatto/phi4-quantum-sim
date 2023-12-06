4842e7393b35b22b908d3edb1184d2c1e6803d47
Dario Gatto <dar.gatto@gmail.com>
2023-12-06 09:32:53 +0000

    Create VSCode workspace & improve Gaussian module readablity

--- gaussian.py
+++ gaussian.py
@@ -1,21 +1,22 @@
-from mpmath import re, exp, acos, sqrt, jtheta
+from mpmath import re, jtheta, exp, acos, sqrt
 from mpmath import j as i
-
 
 # Gaussian state generator subroutine.
 # Classical computation of a specific function of the Gaussian parameters is required
 def JacobiTheta(mean, var):
-    return mp.re(mp.jtheta(3,mean/(mp.j*var**2),mp.exp(-1/var**2))) 
+    return re(jtheta(3, mean/(i*var**2), exp(-1/var**2))) 
 
 # The function is used to determine the qubit rotation angle employed in the algorithm
 def KitaevAngle(mean, var):
-    return mp.acos(mp.sqrt(JacobiTheta(mean/2,var/2)/JacobiTheta(mean,var)))
+    cos_alpha_sqrd = JacobiTheta(mean/2, var/2) / JacobiTheta(mean, var)
+    alpha = acos(sqrt(cos_alpha_sqrd))
+    return float(alpha)
 
 # The actual state preparation subroutine is defined recursively
-def discr_gaussian(qbits, mean, var):
+def KitaevGaussian(qbits, mean, var):
     rout = QRoutine()
     wires = rout.new_wires(qbits)
-    rout.apply(RY(2*float(KitaevAngle(mean,var))), wires[0]) #Rotate leftmost qubit. The angle must be converted to float for the quantum gate to accept it
+    rout.apply(RY(2*KitaevAngle(mean,var)), wires[0]) #Rotate leftmost qubit. The angle must be converted to float for the quantum gate to accept it
     if qbits>1:
         rout.apply(X, wires[0]) #Switch leftmost bit logical values
         rout.apply(discr_gaussian(qbits-1, mean/2, var/2).ctrl(), wires[0], wires[1:qbits]) #Complete one half of the state
